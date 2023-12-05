5e3cb12ded541a0b7314f413029dbff9a8bce9b3
Jason Cheng <jasoncky96@gmail.com>
2023-12-05 23:21:41 +0800

    added jpa dependency and postgres on docker-compose

---
+++ src/main/java/com/example/demo/domain/Author.java
@@ -0,0 +1,26 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "authors")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_id_seq")
+    private long id;
+    private String name;
+    private Integer age;
+}
